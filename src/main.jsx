e497a5ddce15e2426cca7bc461323e4b8aa15c02
Adao Iturrusgarai <adaoiturrusgarai@Adaos-MacBook-Air.local>
2026-03-15 11:27:59 -0300

    aaaaaaa

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
-import { registerSW } from "virtual:pwa-register"
-
-registerSW({
-  immediate: true
-})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -16,3 +11,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 )
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
